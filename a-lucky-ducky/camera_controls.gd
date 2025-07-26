8214be4cb0f9420fb9ceb8dade5d4ac9e5dc41a3
Armando Martinez <martinez.armando508w@gmail.com>
2025-07-26 01:25:43 -0600

    adjust file structure

--- a-lucky-ducky/camera_controls.gd
+++ a-lucky-ducky/camera_controls.gd
@@ -3,6 +3,7 @@ extends Node3D
 @export var rotation_speed: float = 2.0
 @export var current_rotation: float = 0
 @export var camera_speed: float = 0.3
+@export var camera_rotation_amount: float = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -22,14 +23,14 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("Zoom_in"):
 		rotation.x -= rotation_speed * delta
 		
-	if Input.is_action_pressed("Arrow_right"):
+	if Input.is_action_just_pressed("Arrow_right"):
 		var tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_EXPO)
-		current_rotation += 0.5
+		current_rotation += camera_rotation_amount
 		tween.tween_property(self, "rotation:y", current_rotation, camera_speed)
 		
-	if Input.is_action_pressed("Arrow_left"):
+	if Input.is_action_just_pressed("Arrow_left"):
 		var tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_EXPO)
-		current_rotation -= 0.5
+		current_rotation -= camera_rotation_amount
 		tween.tween_property(self, "rotation:y", current_rotation, camera_speed)	
 
 	
