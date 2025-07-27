e289653995871cd5a99dd8df00d23ec5be2c4bc5
Armando Martinez <martinez.armando508w@gmail.com>
2025-07-26 23:43:02 -0600

    add water and adjust lighting

--- a-lucky-ducky/camera_controls.gd
+++ a-lucky-ducky/camera_controls.gd
@@ -1,9 +1,11 @@
 extends Node3D
 
 @export var rotation_speed: float = 2.0
+@export var camera_speed: float = 2.0
 @export var current_rotation: float = 0
-@export var camera_speed: float = 0.3
+@export var camera_rotation_speed: float = 0.5
 @export var camera_rotation_amount: float = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,26 +14,30 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("Left"):
-		rotation.y += rotation_speed * delta
-		
+		#rotation.y += rotation_speed * delta
+		position.x -= camera_speed * delta
 	if Input.is_action_pressed("Right"):
-		rotation.y -= rotation_speed * delta
+		#rotation.y -= rotation_speed * delta
+		position.x += camera_speed * delta
+
 		
-	if Input.is_action_pressed("Zoom_out"):
-		rotation.x += rotation_speed * delta
-		
-	if Input.is_action_pressed("Zoom_in"):
-		rotation.x -= rotation_speed * delta
-		
+	if Input.is_action_pressed("Up"):
+		#rotation.x += rotation_speed * delta
+		position.y += camera_speed * delta
+
+	if Input.is_action_pressed("Down"):
+		#rotation.x -= rotation_speed * delta
+		position.y -= camera_speed * delta
+
 	if Input.is_action_just_pressed("Arrow_right"):
 		var tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_EXPO)
 		current_rotation += camera_rotation_amount
-		tween.tween_property(self, "rotation:y", current_rotation, camera_speed)
+		tween.tween_property(self, "rotation:y", current_rotation, camera_rotation_speed)
 		
 	if Input.is_action_just_pressed("Arrow_left"):
 		var tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_EXPO)
 		current_rotation -= camera_rotation_amount
-		tween.tween_property(self, "rotation:y", current_rotation, camera_speed)	
+		tween.tween_property(self, "rotation:y", current_rotation, camera_rotation_speed)	
 
 	
 	
