bc0a44eebb3fdfe73581ff379cf5c043eb5630e3
Armando Martinez <martinez.armando508w@gmail.com>
2025-07-30 18:06:18 -0600

    add microcontroller functions

---
+++ a-lucky-ducky/scripts/SimpleSerialController.cs
@@ -0,0 +1,62 @@
+using Godot;
+using System;
+using System.IO.Ports;
+
+public partial class SimpleSerialController : Node
+{
+	private SerialPort _serialPort;
+	[Signal] 
+	public delegate void SerialDataReceivedEventHandler(string data);
+	[Signal]
+	public delegate void SerialErrorEventHandler(string error);
+	private string _serialDataReceived;
+
+	public override void _Ready()
+	{
+		GD.Print(SerialPort.GetPortNames());
+		
+		string portName = "/dev/cu.usbmodem1301"; 
+		int baudRate = 9600;
+		
+
+		_serialPort = new SerialPort(portName, baudRate);
+		_serialPort.ReadTimeout = 10; 
+		try
+		{
+			_serialPort.Open();
+			GD.Print($"Successfully opened port {portName}.");
+		}
+		catch (Exception ex)
+		{
+			GD.PrintErr($"Could not open serial port: {ex.Message}");
+			EmitSignal(SignalName.SerialError, ex.Message);
+		}
+	}
+
+	public override void _Process(double delta)
+	{
+		if (_serialPort != null && _serialPort.IsOpen)
+		{
+			try
+			{
+				if (_serialPort.BytesToRead > 0)
+				{
+					_serialDataReceived = _serialPort.ReadLine();
+					GD.Print(_serialDataReceived);
+					EmitSignal(SignalName.SerialDataReceived, _serialDataReceived);
+				}
+			}
+			catch (TimeoutException)
+			{
+			}
+		}
+	}
+
+	public override void _ExitTree()
+	{
+		if (_serialPort != null && _serialPort.IsOpen)
+		{
+			_serialPort.Close();
+		}
+	}
+}
---
+++ a-lucky-ducky/scripts/testCubeScript.gd
@@ -0,0 +1,20 @@
+extends MeshInstance3D
+var cubeMaterial
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_simple_serial_controller_serial_data_received(data: String) -> void:
+	get_surface_override_material(0).albedo_color = Color(0, 1, 1)
+	print("Data received")
+
+
+func _on_simple_serial_controller_serial_error(error: String) -> void:
+	get_surface_override_material(0).albedo_color = Color(1, 0, 0)
+	print(error)
