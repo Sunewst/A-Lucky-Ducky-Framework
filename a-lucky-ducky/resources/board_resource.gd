044564e6da8fcbeea8b51aa78f558fca09a253ea
Armando Martinez <martinez.armando508w@gmail.com>
2025-08-25 17:54:29 -0600

    Create a board resource template

---
+++ a-lucky-ducky/resources/board_resource.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name board_resource
+
+@export var board_name: String
+@export var board_FQBN: String
+@export var board_core: String
+@export var board_author: String
+@export var board_url: String
+
+@export var unique_code_completion: Array[String]
