f5e58bbefb4895d860453adf0158d7a9b61843ad
Armando Martinez <martinez.armando508w@gmail.com>
2025-12-10 15:40:36 -0700

    Create FastLED cs script

---
+++ experimental/scripts/FastLED.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class FastLED : Node
+{
+    public static void show()
+    {
+        
+    }
+    
+    public static void addLeds()
+    {
+        
+    }
+}
