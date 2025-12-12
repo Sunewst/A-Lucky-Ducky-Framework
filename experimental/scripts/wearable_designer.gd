4a019c0ba12cb1023c52171b3def7592665cc93c
Armando Martinez <martinez.armando508w@gmail.com>
2025-12-12 15:32:53 -0700

    Add debug leds array for testing

--- experimental/scripts/wearable_designer.gd
+++ experimental/scripts/wearable_designer.gd
@@ -1,9 +1,10 @@
 extends Node3D
 
+@export var debug_leds: PackedColorArray
+
 @onready var code_edit_node: CodeEdit
 @onready var fastled_engine = %FastLEDEngine
 
-var leds: PackedColorArray = [Color(0,0,0)]
 
 func _ready() -> void:
 	if not find_child("CodeEdit") == null:
@@ -13,4 +14,5 @@ func _ready() -> void:
 
 func _compile_fastled() -> void:
 	#print(FastLEDParser.parse_code(code_edit_node))
+	fastled_engine.addLeds(debug_leds)
 	fastled_engine.show()
